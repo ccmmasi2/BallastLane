760dfdfa695a9ff852aadc311bb1649d13d0e2c8
ccmmasi <144745598+ccmmasi2@users.noreply.github.com>
2026-05-23 11:22:19 -0500

    first upload

---
+++ BallastLane.Test.Solution/BallastLane.Test.Core/DTO/InvoiceHDRDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BallastLane.Test.Core.DTO
+{
+    internal class InvoiceHDRDTO
+    {
+    }
+}
