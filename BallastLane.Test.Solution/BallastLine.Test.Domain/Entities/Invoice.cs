619abddd47b82e4d7b18badbb04e6c41dbb157c6
cmontenegro <ext.cmontenegro@maxillc.com>
2026-05-23 13:51:24 -0500

    domain

---
+++ BallastLane.Test.Solution/BallastLine.Test.Domain/Entities/Invoice.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BallastLine.Test.Domain.Entities
+{
+    internal class Invoice
+    {
+    }
+}
