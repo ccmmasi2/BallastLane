5e90074c9ec72b9dbcca08f1fe4bf83fa95a9f11
cmontenegro <ext.cmontenegro@maxillc.com>
2026-05-23 13:39:13 -0500

    Distribution

---
+++ BallastLane.Test.Solution/BallastLane.Test.Infrastructure/ObjectRepository/Interface/ICategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BallastLane.Test.Infrastructure.ObjectRepository.Interface
+{
+    internal class ICategoryRepository
+    {
+    }
+}
