1133bca65d2bb7ad5af2c3e88a9ca03307c696b1
cmontenegro <ext.cmontenegro@maxillc.com>
2026-05-24 10:44:28 -0500

    dependency injection

--- BallastLane.Test.Solution/BallastLane.Test.API/Extensions/ServiceCollectionExtensions.cs
+++ BallastLane.Test.Solution/BallastLane.Test.API/Extensions/ServiceCollectionExtensions.cs
@@ -5,6 +5,9 @@ using BallastLane.Test.Infrastructure.Authentication;
 using BallastLane.Test.Infrastructure.Data;
 using BallastLane.Test.Infrastructure.Repositories.Implementations;
 using BallastLane.Test.Infrastructure.Repositories.Interfaces;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace BallastLane.Test.API.Extensions
 {
@@ -19,15 +22,18 @@ namespace BallastLane.Test.API.Extensions
             services.AddScoped<ICustomerService, CustomerService>();
             services.AddScoped<IInvoiceService,  InvoiceService>();
 
-            services.AddSingleton<CategoryValidator>();
-            services.AddSingleton<ProductValidator>();
-            services.AddSingleton<CustomerValidator>();
-            services.AddSingleton<InvoiceValidator>();
-            services.AddSingleton<RegisterValidator>();
-            services.AddSingleton<LoginValidator>();
+            return services;
+        }
 
-            services.AddSingleton<PasswordHasher>();
-            services.AddScoped<JwtTokenGenerator>();
+        public static IServiceCollection AddValidators(
+            this IServiceCollection services)
+        {
+            services.AddScoped<CategoryValidator>();
+            services.AddScoped<ProductValidator>();
+            services.AddScoped<CustomerValidator>();
+            services.AddScoped<InvoiceValidator>();
+            services.AddScoped<RegisterValidator>();
+            services.AddScoped<LoginValidator>();
 
             return services;
         }
@@ -36,12 +42,44 @@ namespace BallastLane.Test.API.Extensions
             this IServiceCollection services)
         {
             services.AddSingleton<DbConnectionFactory>();
+            services.AddScoped<DatabaseInitializer>();
 
             services.AddScoped<IUserRepository,     UserRepository>();
             services.AddScoped<ICategoryRepository, CategoryRepository>();
             services.AddScoped<IProductRepository,  ProductRepository>();
             services.AddScoped<ICustomerRepository, CustomerRepository>();
             services.AddScoped<IInvoiceRepository,  InvoiceRepository>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddJwtAuthentication(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            var secret   = configuration["Jwt:Secret"]   ?? throw new InvalidOperationException("Jwt:Secret is not configured.");
+            var issuer   = configuration["Jwt:Issuer"]   ?? throw new InvalidOperationException("Jwt:Issuer is not configured.");
+            var audience = configuration["Jwt:Audience"] ?? throw new InvalidOperationException("Jwt:Audience is not configured.");
+
+            services.AddSingleton<PasswordHasher>();
+            services.AddScoped<JwtTokenGenerator>();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer           = true,
+                        ValidateAudience         = true,
+                        ValidateLifetime         = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer              = issuer,
+                        ValidAudience            = audience,
+                        IssuerSigningKey         = new SymmetricSecurityKey(
+                            Encoding.UTF8.GetBytes(secret)),
+                        ClockSkew                = TimeSpan.Zero
+                    };
+                });
 
             return services;
         }
--- BallastLane.Test.Solution/BallastLane.Test.API/Program.cs
+++ BallastLane.Test.Solution/BallastLane.Test.API/Program.cs
@@ -1,42 +1,20 @@
 using BallastLane.Test.API.Extensions;
 using BallastLane.Test.Infrastructure.Data;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
-builder.Services.AddApplicationServices();
 builder.Services.AddRepositories();
-
-builder.Services.AddScoped<DatabaseInitializer>();
-
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer           = true,
-            ValidateAudience         = true,
-            ValidateLifetime         = true,
-            ValidateIssuerSigningKey = true,
-            ValidIssuer              = builder.Configuration["Jwt:Issuer"],
-            ValidAudience            = builder.Configuration["Jwt:Audience"],
-            IssuerSigningKey         = new SymmetricSecurityKey(
-                Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Secret"]!))
-        };
-    });
+builder.Services.AddValidators();
+builder.Services.AddApplicationServices();
+builder.Services.AddJwtAuthentication(builder.Configuration);
 
 var app = builder.Build();
 
 using (var scope = app.Services.CreateScope())
-{
-    var initializer = scope.ServiceProvider.GetRequiredService<DatabaseInitializer>();
-    await initializer.InitializeAsync();
-}
+    await scope.ServiceProvider.GetRequiredService<DatabaseInitializer>().InitializeAsync();
 
 if (app.Environment.IsDevelopment())
     app.MapOpenApi();
